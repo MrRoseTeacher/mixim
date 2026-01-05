e504f42f86052a2fb3ca12f2ed0c57bf71d78fff
Rose <TV58777@tvdsb.ca>
2026-01-05 09:30:05 -0500

    fixed clearing of link

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ eid("gen-mixim").onclick = function(){
     const fullURL = domain + "?data=" + query
     
     //populate the link box below
-    eid("mixim-link").innerHTML = '<button id="copy-link"><span class="material-symbols-outlined">content_copy</span></button>';
+    eid("mixim-link").querySelector('a').remove;
     const link = document.createElement("a");
     link.innerHTML = fullURL;
     link.href = fullURL;
